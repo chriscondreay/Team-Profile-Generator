df7f4b1c4d33e1464fc2409bbce9fb7ea585adae
The Instigator <condreaychris@gmail.com>
2021-11-26 15:29:03 -0800

    descriptions and its

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,19 +1,9 @@
-const Engineer = require("../lib/Engineer");
+const Engineer = require('../lib/Engineer');
 
-test("Can set GitHUb account via constructor", () => {
-  const testValue = "GitHubUser";
-  const e = new Engineer("Foo", 1, "test@test.com", testValue);
-  expect(e.github).toBe(testValue);
-});
-
-test("getRole() should return \"Engineer\"", () => {
-  const testValue = "Engineer";
-  const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
-  expect(e.getRole()).toBe(testValue);
-});
-
-test("Can get GitHub username via getGithub()", () => {
-  const testValue = "GitHubUser";
-  const e = new Engineer("Foo", 1, "test@test.com", testValue);
-  expect(e.getGithub()).toBe(testValue);
-});
+describe("Engineer", () => {
+  it('should display engineer name as string', () => {
+    const obj = new Engineer();
+    expect("github" in obj).toEqual(true);
+    expect(typeof obj.github === string).toEqual(true);
+  })
+})
--- test/Intern.test.js
+++ test/Intern.test.js
@@ -1,19 +1,10 @@
-const Intern = require("../lib/Intern");
+const Intern = require('../lib/Intern');
+const inquirer = require("./inquirer");
 
-test("Can set school via constructor", () => {
-  const testValue = "UCLA";
-  const e = new Intern("Foo", 1, "test@test.com", testValue);
-  expect(e.school).toBe(testValue);
-});
-
-test("getRole() should return \"Intern\"", () => {
-  const testValue = "Intern";
-  const e = new Intern("Foo", 1, "test@test.com", "UCLA");
-  expect(e.getRole()).toBe(testValue);
-});
-
-test("Can get school via getSchool()", () => {
-  const testValue = "UCLA";
-  const e = new Intern("Foo", 1, "test@test.com", testValue);
-  expect(e.getSchool()).toBe(testValue);
-});
+describe("Intern", () => {
+  it('should display intern name as string', () => {
+    const obj = new Intern();
+    expect("school" in obj).toEqual(true);
+    expect(typeof obj.school === string).toEqual(true);
+  })
+})
