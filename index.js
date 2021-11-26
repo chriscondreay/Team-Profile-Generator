bf4addf6d521f7d1b0d6b7cdd15c2a31bf74d8ac
The Instigator <condreaychris@gmail.com>
2021-11-26 15:36:01 -0800

    jobQuestion variable inputs

--- index.js
+++ index.js
@@ -4,3 +4,32 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
+const jobQuestions = [
+  {
+    type: 'input',
+    message: "What is your employee's name?",
+    name: 'name'
+  },
+  {
+    type: "input",
+    message: "What is your employee's ID number?",
+    name: "ID"
+  },
+  {
+    type: "input",
+    message: "What is your employee's email address",
+    name: "email",
+  },
+  {
+    type: "list",
+    message: "What is the job you are assigning to your employee?",
+    name: "choice",
+    choices: [
+      "Manager",
+      "Intern",
+      "Engineer"
+    ],
+  }
+];
+
+
